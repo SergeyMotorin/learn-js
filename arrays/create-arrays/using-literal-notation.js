e26fd8a536e8bcded7ee060e65a7a7bb55025aac
Vladimir Pavlov <pavloff.com@gmail.com>
2022-04-25 00:12:19 +0500

    Remove semicolons

--- arrays/create-arrays/using-literal-notation.js
+++ arrays/create-arrays/using-literal-notation.js
@@ -1,14 +1,14 @@
 
 // Create arrays using literal notation
 
-const fruits = ['🍊', '🍐', '🍌', '🍏'];
-console.log(fruits.length + ' elements');
+const fruits = ['🍊', '🍐', '🍌', '🍏']
+console.log(fruits.length + ' elements')
 // Output: 4 elements
 
-const numbers = [10, 20, 100];
-console.log(numbers.length + ' elements');
+const numbers = [10, 20, 100]
+console.log(numbers.length + ' elements')
 // Output: 3 elements
 
-const data = [10, 'Some text'];
-console.log(data.length + ' elements');
+const data = [10, 'Some text']
+console.log(data.length + ' elements')
 // Output: 2 elements
