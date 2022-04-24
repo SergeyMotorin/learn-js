940e66495217876102342353bebffe0cab9bf39e
Vladimir Pavlov <pavloff.com@gmail.com>
2022-04-25 03:13:29 +0500

    Implementation removeDuplicates using Array.filter()

---
+++ arrays/remove-duplicates/using-index-of.js
@@ -0,0 +1,15 @@
+
+// Implementation of the function using Array.filter()
+
+Array.prototype.removeDuplicates = function() {
+    return this.filter(
+        (item, index) => {
+            return this.indexOf(item) === index
+        }
+    )
+}
+
+const data = [ 30, 10, 10, 20, 10, 20, 30 ]
+
+const newData = data.removeDuplicates()
+console.log(newData)
