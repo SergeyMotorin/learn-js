e5c40f5a9efbf37543591f6ffd8ce52bc6454d8f
Vladimir Pavlov <pavloff.com@gmail.com>
2022-04-27 00:13:03 +0500

    arrays, create arrays: add 'using-typed-array-objects.js'

---
+++ arrays/01-create-arrays/using-typed-array-objects.js
@@ -0,0 +1,58 @@
+
+// Value Range: -128 to 127
+
+const int8 = new Int8Array()
+
+
+// Value Range: 0 to 255
+
+const uint8 = new Uint8Array()
+
+
+// Value Range: 0 to 255
+
+const uint8Clamped = new Uint8ClampedArray()
+
+
+// Value Range: -32768 to 32767
+
+const int16 = new Int16Array()
+
+
+// Value Range: 0 to 65535
+
+const uint16 = new Uint16Array()
+
+
+// Value Range: -2147483648 to 2147483647
+
+const int32 = new Int32Array()
+
+
+// Value Range: 0 to 4294967295
+
+const uint32 = new Uint32Array()
+
+
+// Value Range: -3.4E38 to 3.4E38 and 1.2E-38 is the min positive number
+
+const float32 = new Float32Array()
+
+
+// Value Range: -1.8E308 to 1.8E308 and 5E-324 is the min positive number
+
+const float64 = new Float64Array()
+
+
+// Value Range: -2^63 to 2^63 - 1
+
+const bigint64 = new BigInt64Array()
+
+
+// Value Range: 0 to 2^64 - 1
+
+const biguint64 = new BigUint64Array()
+
+
+
+
