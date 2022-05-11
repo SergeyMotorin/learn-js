8a2d0a142b851adcdc79d4d6e80620f35ee094e5
Vladimir Pavlov <pavloff.com@gmail.com>
2022-05-11 18:04:59 +0500

    Arrays, Basic operations: Add "traversial.js"

---
+++ arrays/02-basic-operations/traversial.js
@@ -0,0 +1,18 @@
+
+// Simple Array traversing
+
+// Traverse an array directly from the array index
+
+const words = ["Hello", "World"]
+
+console.log(words[0])
+console.log(words[1])
+
+
+// Traverse an array using for loop
+
+const fruits = [ "🍊", "🍐", "🍌", "🍏" ]
+  
+for (var i = 0; i < fruits.length; i++) {  
+  console.log(fruits[i]);  
+}
