093e79121a3532b3e2a2b2517e6b94920e57dbd6
Vladimir Pavlov <pavloff.com@gmail.com>
2022-05-11 18:04:23 +0500

    Arrays, Basic operations: Add code that removes items using Array.prototype.filter()

--- arrays/02-basic-operations/removing.js
+++ arrays/02-basic-operations/removing.js
@@ -1,9 +1,9 @@
 
-// Remove an array element using "delete" operator 
+// Remove an array item using "delete" operator 
 
-const fruits = ["🍊", "🍏", "🍊"];
-delete fruits[1]; 
-console.log(fruits);
+const fruits = ["🍊", "🍏", "🍊"]
+delete fruits[1]
+console.log(fruits)
 // Output: [ "🍊", <1 empty item>, "🍊" ]
 
 // Add delete() function to Array.Prototype
@@ -11,38 +11,45 @@ console.log(fruits);
 if (!Object.hasOwnProperty.call(Array.prototype, "delete")) {
   Object.defineProperty(Array.prototype, "delete", {
     value: function (index) {
-      delete this[index]; 
-      return this;
+      delete this[index] 
+      return this
     }
   }); 
 }
 
-const trees = ["🌲", "🍏", "🌲"];
-const newTrees = trees.delete(1);
+const trees = ["🌲", "🍏", "🌲"]
+const newTrees = trees.delete(1)
 
-console.log(newTrees);
+console.log(newTrees)
 // Output: [ "🌲", <1 empty item>, "🌲" ]
 
 
 // Remove from the end of an array 
 
-// Array elements can be removed from the end of an array by setting the length property to a value less than the current value. 
+// Array items can be removed from the end of an array by setting the length property to a value less than the current value
 
-var ascendingNumbers = [1, 2, 3, 4, 5, 6];
+var ascendingNumbers = [1, 2, 3, 4, 5, 6]
     
-// Set length to remove elements
-ascendingNumbers.length = 5; 
+// Set length to remove items
+ascendingNumbers.length = 5
 
-console.log(ascendingNumbers); 
+console.log(ascendingNumbers)
 // Output: [ 1, 2, 3, 4, 5 ]
 
 
-// Remove elements using splice()
+// Remove items using Array.prototype.slice()
 
-const vehicles = ["🚘", "🛩", "🚊", "🍏", "🛳"];
-vehicles.splice(3, 1);
+const vehicles = ["🚘", "🛩", "🚊", "🍏", "🛳"]
+vehicles.splice(3, 1)
 
-console.log(vehicles); 
-// [ "🚘", "🛩", "🚊", "🛳" ]
+console.log(vehicles)
+// Output: [ "🚘", "🛩", "🚊", "🛳" ]
 
 
+// Remove items using Array.prototype.filter() 
+
+const differentNumbers = [7, 3, 5, 1, 2, 6, 4, 9, 0, 8]
+const filteredNumbers = differentNumbers.filter(value => (value > 5))
+
+console.log(filteredNumbers)
+// Output: [ 7, 6, 9, 8 ]
